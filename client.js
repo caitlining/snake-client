3ed3fc94bf918bdd31b1644984fa4122a0689dd4
caitlining <caitlincing@gmail.com>
2019-09-25 21:13:17 +0000

    Changing code to include moving up and moving down, but then removed because it was hard coded

--- client.js
+++ client.js
@@ -19,7 +19,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: CCI');
-  })
+  });
 
   return conn;
 }
