d09019afda9bfb26900c54154408ba58bb87a0a2
caitlining <caitlincing@gmail.com>
2019-09-25 21:46:13 +0000

    Making a file to hold all of the code for handling user input, also modifying the play file to require the necessary functionality from input

---
+++ input.js
@@ -0,0 +1,42 @@
+let connection;
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function (key) {
+
+ if (key === '\u0003' || key === 'q') {
+ process.exit();
+ }; 
+
+ if (key === 'w') {
+   connection.write("Move: up");
+ };
+
+ if (key === "a") {
+   connection.write("Move: left");
+ };
+
+ if (key === "s") {
+   connection.write("Move: down");
+ };
+
+ if(key === "d") {
+   connection.write("Move: right");
+ }
+
+
+}
+
+module.exports = { setupInput };
