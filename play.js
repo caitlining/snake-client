63a397d0014df7f1a8620cec6e46468154d1b948
caitlining <caitlincing@gmail.com>
2019-09-25 20:39:32 +0000

    Made a new client file to hold the function that allows us to handle our TCP connection, also removed this code from play.js, and used the export/require functions to access all that we need

--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.43.245',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  //event handler to handle incoming data and console log it for me, the player
-  conn.on('data', (data) => {
-    console.log('Server says:', data);
-  });
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting...');
 connect();
