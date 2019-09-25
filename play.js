3a856cc3fd0751a690b7cc35f3e7df556f28c12b
caitlining <caitlincing@gmail.com>
2019-09-25 20:35:10 +0000

    Adding code to make my client connect to the main snake screen and also handle incoming data from the server

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.43.245',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  
+  //event handler to handle incoming data and console log it for me, the player
+  conn.on('data', (data) => {
+    console.log('Server says:', data);
+  });
+
+  return conn;
+}
+
+console.log('Connecting...');
+connect();
